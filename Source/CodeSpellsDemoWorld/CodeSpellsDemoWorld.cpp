ccbe9d5337533d7be474aa4e83cf6d24eddc4fd2
Lindsey Handley <lindseyhandley@gmail.com>
2021-02-05 17:35:37 +0000

    first

---
+++ Source/CodeSpellsDemoWorld/CodeSpellsDemoWorld.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CodeSpellsDemoWorld.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CodeSpellsDemoWorld, "CodeSpellsDemoWorld" );
---
+++ Source/CodeSpellsDemoWorld/CodeSpellsDemoWorldGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CodeSpellsDemoWorldGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CODESPELLSDEMOWORLD_API ACodeSpellsDemoWorldGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
